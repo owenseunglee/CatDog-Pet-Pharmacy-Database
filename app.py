5ab3fa57afbbef8419c29cf8d49a5120381f10c8
Jessica Ly <lyjes@flip3.engr.oregonstate.edu>
2024-02-13 10:50:25 -0800

    updating files per flask guide

--- app.py
+++ app.py
@@ -24,22 +24,24 @@ def root():
 
     return render_template("main.j2")
 
-@app.route("/vetenerians", methods=["POST", "GET"])
-def browse_vets():
-    if request.method == "GET":
+############ IGNORE THIS PART ###############
+#@app.route("/veterinarians", methods=["POST", "GET"])
+#def browse_vets():
+#    if request.method == "GET":
 
-        query = "SELECT vet_ FROM Vets"
-        cur = mysql.connection.cursor()
-        cur.execute(query)
-        results = cur.fetchall()
-        print(results)
+#        query = "SELECT vet_ FROM Vets"
+#        cur = mysql.connection.cursor()
+#        cur.execute(query)
+#        results = cur.fetchall()
+#        print(results)
         #query2 = "SELECT id_owner, name FROM Owners"
         #cur = mysql.connection.cursor()
         #cur.execute(query2)
         #owners_data = cur.fecthall()
         
-        return render_template("vets.j2", Vets=results)
+ #       return render_template("vets.j2", Vets=results)
 
+# veterinarian page
 @app.route("/vets")
 def vets():
     query = "SELECT * FROM Vets;"
@@ -48,20 +50,42 @@ def vets():
 
     return render_template("vets.j2", Vets=results)
 
+# adding vet page
 @app.route("/add_vet")
 def add_vets():
     
     return render_template("add_vet.j2")
 
+# deleting vet page
 @app.route("/del_vet")
 def delete_vets():
 
     return render_template("del_vet.j2")
 
+# owners page
+@app.route("/owners")
+def owners():
+    query = "SELECT * FROM Owners;"
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+    results = cursor.fetchall()
+
+    return render_template("owners.j2", Owners=results)
+
+# adding owner page
+@app.route("/add_owner")
+def add_owner():
+    
+    return render_template("add_owner.j2")
+
+# deleting owner page
+@app.route("/del_owner")
+def del_owner():
+
+    return render_template("del_owner.j2")
+
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 9112))
+    port = int(os.environ.get('PORT', 8200))
 
 
     app.run(port=port, debug = True)
-
