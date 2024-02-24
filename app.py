a89be4ee92828bebd892206e3e0f778b494e3286
Owen Lee <leeowe@flip3.engr.oregonstate.edu>
2024-02-23 20:04:11 -0800

    changing tables

--- app.py
+++ app.py
@@ -189,7 +189,7 @@ def add_prescriptMeds():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 58580)) 
+    port = int(os.environ.get('PORT', 58581)) 
     #                                 ^^^^
     #              You can replace this number with any valid port
     
