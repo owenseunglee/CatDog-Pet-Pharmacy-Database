c730a16e0d7772e63c2bf05aa1872714d5b260ae
Owen <owenseunglee@gmail.com>
2024-02-27 16:44:59 -0800

    vet_add backend

--- app.py
+++ app.py
@@ -30,32 +30,21 @@ def root():
 
     return render_template("main.html", title='Home')
 
-############ IGNORE THIS PART ###############
-#@app.route("/veterinarians", methods=["POST", "GET"])
-#def browse_vets():
-#    if request.method == "GET":
-
-#        query = "SELECT vet_ FROM Vets"
-#        cur = mysql.connection.cursor()
-#        cur.execute(query)
-#        results = cur.fetchall()
-#        print(results)
-        #query2 = "SELECT id_owner, name FROM Owners"
-        #cur = mysql.connection.cursor()
-        #cur.execute(query2)
-        #owners_data = cur.fecthall()
-        
- #       return render_template("vets.j2", Vets=results)
-
 # veterinarian page
 @app.route("/vets")
 def vets():
-    # query = "SELECT * FROM Vets;"
-    # cursor = db.execute_query(db_connection=db_connection, query=query)
-    # results = cursor.fetchall()
+   if request.method == "GET":
+       query = "SELECT * FROM Vets;"
+       cursor = db.execute_query(db_connection=db_connection, query=query)
+       results = cursor.fetchall()
 
-    # return render_template("vets/vets.j2", Vets=results)
-    return render_template("vets/vets.html", title='Veterinarians')
+       query2 = query = "SELECT id_vet, CONCAT(Vets.name, ' (', id_vet, ')') AS vet_name_id FROM Vets;"
+       cursor = db.execute_query(db_connection=db_connection, query=query2)
+       vet_results = cursor.fetchall()
+       print("Vets_Dropdown:", vet_results) 
+       print("Vets", results)
+
+   return render_template("vets/vets.html", title='Veterinarians', Vets=results, Vets_Dropdown=vet_results)
 
 
 # adding vet page
@@ -70,9 +59,18 @@ def delete_vets():
 
     return render_template("vets/del_vet.j2")
 
-@app.route("/edit_vet")
-def edit_vet():
-    return render_template("vets/edit_vet.j2")
+@app.route("/edit_vet/<int:id_vet>", methods=['GET', 'POST'])
+def edit_vet(id_vet):
+    if request.method == 'POST':
+        name = request.form['name']
+        clinic = request.form['clinic']
+        email = request.form['email']
+        no_of_patients = request.form['no_of_patients']
+
+        query = ("UPDATE Vets SET name = %s, clinic = %s, email =%s, no_of_patients = %s WHERE id_vet = %s", (name, clinic, email, no_of_patients, id_vet))
+        db.commit()
+        return redirect("vets/vets.html")
+    #return render_template("vets/edit_vet.j2")
 
 # owners page
 @app.route("/owners")
