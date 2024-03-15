5ca91e6e42f12a48b096d9404b49c57cb57b5d56
Owen <owenseunglee@gmail.com>
2024-03-15 16:42:46 -0700

    footer

--- app.py
+++ app.py
@@ -631,7 +631,7 @@ def add_prescriptMeds():
         return render_template("intersection/add_prescriptMeds.html", Prescriptions_Dropdown=prescription_results, Medications_Dropdown=med_results)
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 58569)) 
+    port = int(os.environ.get('PORT', 58511)) 
      #                               ^^^^
     #             You can replace this number with any valid port
     
